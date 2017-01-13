e40af5a9d3dd4f69d8fe42d9c00dd7e03dffd33b
Raphael Khaiat <raphael@khaiat.org>
2017-01-13 19:08:49 +0100

    remove hack for kalliope rest api url to get synapses

--- app/adapters/synapse.js
+++ app/adapters/synapse.js
@@ -6,15 +6,6 @@ export default DS.RESTAdapter.extend({
   headers: {
     withCredentials: true,
     Authorization: 'Basic ' + btoa(ENV.APP.KALLIOPE.LOGIN + ':' + ENV.APP.KALLIOPE.PASS),
-  },
-  // This is a hack as kalliope doesn't answer to /synapses but only to /synapses/.
-  buildURL: function(modelName, id, snapshot, requestType, query) {
-    var url = this._buildURL(modelName, id);
-    if (modelName == 'synapse' && !id) {
-      //url = url + '/';
-    }
-
-    return url;
   }
 });
 
